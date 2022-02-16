5140eeb9251cf445a3ab27a150c85cb7715c7f62
NoHomeLacey <37221916+NoHomeLacey@users.noreply.github.com>
2022-02-16 15:23:54 +0000

    Update keymap.c
    Updated Layer 1.

--- keyboards/senselessclay/ck60/keymaps/via/keymap.c
+++ keyboards/senselessclay/ck60/keymaps/via/keymap.c
@@ -31,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(1),   KC_RCTL
     ),
-    [_1] = LAYOUT_60_iso(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   _______,
-        KC_CAPS, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, _______, KC_INS,  KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  KC_END,  KC_PGDN, KC_DOWN,          _______,
+     [_1] = LAYOUT_60_iso(
+         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, _______,  KC_UP,  _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_VOLD, KC_VOLU,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP, _______, _______, KC_MUTE, _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_END,  KC_PGDN, _______,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
     [_2] = LAYOUT_60_iso(
